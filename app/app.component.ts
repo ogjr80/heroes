72ab7c97e35569baf53e57b6fb7b080f0e28878d
Gideon Ogongo <ogundareayodeji25@giddy-pc.local>
2016-06-05 16:39:15 +0200

    Added Routing but dashboard still to be done

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,90 +1,39 @@
-import { Component} from '@angular/core'; 
-import {HeroDetailComponent} from './hero-detail.component'
-import {Hero} from './Hero'; 
-import { HEROES } from './mock-heroes';
-import {HeroService} from './hero.service';
-import {OnInit} from '@angular/core';
+import {Component} from '@angular/core'; 
+import {HeroesComponent} from './heroes.component'; 
+import {HeroService} from './hero.service'; 
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import {DashboardComponent} from './dashboard.component';
+
 @Component({
     selector: 'my-app', 
-    template: `<h2>My Heroes</h2> 
-               <ul class="heroes">
-                <li *ngFor="let hero of heroes" [class.selected] = "hero===selectedHero" (click)="onSelect(hero)"> 
-                   <span class="badge">{{hero.id}}</span> {{hero.name}}
-                </li>  
-               </ul> 
-               <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-                
-    `,
-    styles:[`
-  .selected {
-    background-color: #CFD8DC !important;
-    color: white;
-  }
-  .heroes {
-    margin: 0 0 2em 0;
-    list-style-type: none;
-    padding: 0;
-    width: 15em;
-  }
-  .heroes li {
-    cursor: pointer;
-    position: relative;
-    left: 0;
-    background-color: #EEE;
-    margin: .5em;
-    padding: .3em 0;
-    height: 1.6em;
-    border-radius: 4px;
-  }
-  .heroes li.selected:hover {
-    background-color: #BBD8DC !important;
-    color: white;
-  }
-  .heroes li:hover {
-    color: #607D8B;
-    background-color: #DDD;
-    left: .1em;
-  }
-  .heroes .text {
-    position: relative;
-    top: -3px;
-  }
-  .heroes .badge {
-    display: inline-block;
-    font-size: small;
-    color: white;
-    padding: 0.8em 0.7em 0 0.7em;
-    background-color: #607D8B;
-    line-height: 1em;
-    position: relative;
-    left: -1px;
-    top: -4px;
-    height: 1.8em;
-    margin-right: .8em;
-    border-radius: 4px 0 0 4px;
-  }
-`],
-directives: [HeroDetailComponent], 
-providers: [HeroService]
-
+    template: `
+        <h2>{{title}}</h2>
+        <a [routerLink] = "['Heroes']">Heroes</a>
+        <a [routerLink] = "['Dashboard']">Dashboard</a>
+        <router-outlet></router-outlet>
+    `, 
+    directives: [ 
+        ROUTER_DIRECTIVES
+        ],
+    providers: [
+        ROUTER_PROVIDERS,
+        HeroService
+        ] 
+    
 })
-export class AppComponent implements OnInit{
-    
-    constructor(private heroService : HeroService){
-      
+@RouteConfig([
+    {
+        path: '/dashboard', 
+        name: 'Dashboard',
+        component: DashboardComponent,
+        useAsDefault: true
+    },
+    {
+        path: '/heroes', 
+        name: 'Heroes', 
+        component: HeroesComponent
     }
-    ngOnInit(){
-        this.getHeroes(); 
-    }
-    getHeroes(){
-        this.heroService.getHeroes().then(heroes => this.heroes=heroes);     
-        }
-    title = 'Heroes Tour Details'; 
-    public heroes: Hero[];
-    selectedHero : Hero; 
-    onSelect(hero: Hero){
-        this.selectedHero = hero; 
-    }
-    
-}
-
+])
+export class AppComponent{
+    title: string = "Tour of Heroes";
+};
